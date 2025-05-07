876048f5e38e470b69a6e4c65e0235fa5d5e24cd
gravitorious <gravitorious98@gmail.com>
2025-05-07 20:41:08 +0300

    Add problem 2

---
+++ 2/2.cpp
@@ -0,0 +1,80 @@
+////created by gravitorious
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#ifdef XOX
+#include "../debug.h"
+#else
+#define debug(...) 77
+#endif
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+ 
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+template <class T>
+void chkmax(T &x,T y){
+    if(x < y) x = y;
+}
+ 
+template <class T>
+void chkmin(T &x,T y){
+    if(x > y) x = y;
+}
+ 
+constexpr int popcnt(int x){
+    return __builtin_popcount(x);
+}
+ 
+constexpr int ctz(int x){
+    return __builtin_ctz(x);
+}
+
+constexpr double int_part(double x, double *intpart){ 
+	return modf(x, intpart); //returns the real part
+}
+ 
+ll ceil_div(ll a, ll b) {
+	return a / b + ((a ^ b) > 0 && a % b);
+}  // divide a by b rounded up
+ll floor_div(ll a, ll b) {
+	return a / b - ((a ^ b) < 0 && a % b);
+}  // divide a by b rounded down
+ 
+long long myRand(long long B) {
+	//0 to B-1
+	return (unsigned long long)rng() % B;
+}
+ 
+int myUniRand(int a, int b){
+	//a to b
+	uniform_int_distribution<int> distribution(a,b);
+	return distribution(rng);
+}
+ 
+void solve(){
+	int fib1 = 1, fib2 = 1;
+	ll sum = 0;
+	const int maxN = (int)4e6;
+	while(true){
+		fib1 = fib1 + fib2;
+		swap(fib1, fib2);
+		if(fib2 > maxN) break;
+		if(fib2 % 2 == 0) sum += fib2;
+	}
+	cout << sum;
+}
+ 
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	solve();
+	return 0;
+}
